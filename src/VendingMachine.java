ddcb5db301dd171a17f4d6f8f33741fc642b21c0
Beep <ln24630@gmail.com>
2020-03-01 22:21:00 -0800

    add some method and constructor

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class VendingMachine {
+    private ArrayList<Product> itemList;
+
+    public VendingMachine() {
+        itemList = new ArrayList<Product>();
+    }
+
+    public void addItem(Product item) {
+        itemList.add(item);
+    }
+
+/*    public Product removeItem(Product item) {
+        return itemList.remove(item);
+    }*/
 }

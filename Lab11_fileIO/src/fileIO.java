7bf01614dc3ab063910654d4d92fc9df69376c10
s-do <48598611+s-do@users.noreply.github.com>
2020-03-05 16:40:03 -0800

    finished lab11

--- Lab11_fileIO/src/fileIO.java
+++ Lab11_fileIO/src/fileIO.java
@@ -1,2 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class fileIO {
+    public static void main(String[] args) throws FileNotFoundException {
+        File inputFile = new File("ziptable.txt");
+        Scanner in = new Scanner(inputFile);
+        PrintWriter out = new PrintWriter("output.txt");
+        while(in.hasNextLine()){
+            out.println(in.nextLine());
+        }
+        in.close();
+        out.close();
+    }
 }

44a82b52bfbf608c9e4967e90048bd2634732f0b
s-do <48598611+s-do@users.noreply.github.com>
2020-03-10 15:19:27 -0700

    handles exceptions with try/catch

--- Lab11_fileIO/src/fileIO.java
+++ Lab11_fileIO/src/fileIO.java
@@ -4,14 +4,30 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class fileIO {
-    public static void main(String[] args) throws FileNotFoundException {
-        File inputFile = new File("ziptable.txt");
-        Scanner in = new Scanner(inputFile);
-        PrintWriter out = new PrintWriter("output.txt");
-        while(in.hasNextLine()){
-            out.println(in.nextLine());
+    public static void main(String[] args) {
+        if (args.length < 2)
+        {
+            System.out.println("Usage: inputFile outputFile");
+            System.exit(0);
         }
-        in.close();
-        out.close();
+        System.out.print("Input file: ");
+        String inputFileName = args[0];
+        System.out.println(args[0]);
+        System.out.print("Output file: ");
+        String outputFileName = args[1];
+        System.out.println(args[1]);
+        
+        File inputFile = new File(inputFileName);
+        try(
+                Scanner in = new Scanner(inputFile);
+                PrintWriter out = new PrintWriter(outputFileName);){
+            while(in.hasNextLine()) {
+                out.println(in.nextLine());
+            }
+            in.close();
+            out.close();
+        } catch (FileNotFoundException fnf){
+            System.out.println(fnf.getMessage());
+        }
     }
 }

c163de0a41255c15cab7e8629dcaeca252df20a6
Neo <ln24630@gmail.com>
2020-03-12 14:09:49 -0700

    add main outline

--- FileIO/src/fileIOtester.java
+++ FileIO/src/fileIOtester.java
@@ -1,7 +1,45 @@
+import java.io.*;
 import java.util.Scanner;
+
 
 public class fileIOtester {
     public static void main(String[] args) {
+        try {
+            File infoFile = new File("infoFile.txt");
+            PrintWriter printWriter = new PrintWriter(new FileWriter(infoFile,true));
+
+            String name = "";
+            while (name.length() < 0) {
+                System.out.println("Enter your name: ");
+                name = getName();
+            }
+
+            System.out.println("Enter type of service: ");
+            String service = getService();
+
+            double saleAmount = 0;
+            while (saleAmount <= 0) {
+                System.out.println("Enter amount of sale: ");
+                saleAmount = getAmount();
+            }
+
+            String date = "";
+            while (date.length() < 0) {
+                System.out.println("Enter date of service: ");
+                date = getDate();
+            }
+
+            //Add all user info to the file(appending)
+            printWriter.printf(name, ";", service, ";",saleAmount, ";", date);
+        }
+        catch (FileNotFoundException fnf){
+            System.out.println(fnf.getMessage());
+        }
+        catch (IOException exception) {
+            //e.printStackTrace();
+            System.out.println("could not open file");
+        }
+
 
     }
 

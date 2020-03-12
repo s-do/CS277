a3268d8c8f56dd17334f5b90a4413326afd2eb30
Neo <ln24630@gmail.com>
2020-03-12 13:32:58 -0700

    Skeleton code for fileIO

---
+++ FileIO/src/fileIOtester.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class fileIOtester {
+    public static void main(String[] args) {
+
+    }
+
+    public static String getName() {
+        return "";
+    }
+
+    public static String getService() {
+        return "";
+    }
+
+    public static double getAmount(){
+        return 0;
+    }
+
+    public static String getDate() {
+        return "";
+    }
+}

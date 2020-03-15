3140df702f7133ff6563e6d9896678874fa08c27
s-do <48598611+s-do@users.noreply.github.com>
2020-03-14 19:18:11 -0700

    filled in methods and validated user input

--- FileIO/src/fileIOtester.java
+++ FileIO/src/fileIOtester.java
@@ -8,63 +8,85 @@ public class fileIOtester {
             File infoFile = new File("sales.txt");
             PrintWriter printWriter = new PrintWriter(new FileWriter(infoFile,true));
 
-            String name = "";
-            while (name.length() < 0) {
-                System.out.println("Enter your name: ");
-                name = getName();
+            // Verifies that name of client is not blank
+            System.out.println("Enter your name: ");
+            String name = getStringInput();
+            while (name.length() <= 0) {
+                System.out.print("Enter your name: ");
+                name = getStringInput();
             }
 
+            // Verifies that service sold is not blank
             System.out.println("Enter type of service: ");
-            String service = getService();
-
-            if (service.equals("Conference")) {
-                throw new UnknownTransactionException("No such service available");
+            String service = getStringInput();
+            while (service.length() <= 0) {
+                System.out.print("Enter type of service: ");
+                service = getStringInput();
             }
 
-            double saleAmount = 0;
+            // Checks if service was sold
+            boolean existingService = isService(service);
+            while (!existingService) {
+                System.out.print("Enter an existing service: ");
+                service = getStringInput();
+                existingService = isService(service);
+            }
+
+            /*if (service.equals("Conference")) {
+                throw new UnknownTransactionException("No such service available");
+            }*/
+
+            // Verifies that amount of sale is positive
+            System.out.println("Enter amount of sale: ");
+            double saleAmount = getAmount();
             while (saleAmount < 0) {
-                System.out.println("Enter amount of sale: ");
+                System.out.print("Enter amount of sale: ");
                 saleAmount = getAmount();
             }
 
-            String date = "";
-            while (date.length() < 0) {
-                System.out.println("Enter date of service: ");
-                date = getDate();
+            // Verifies that date of event is not blank
+            System.out.println("Enter date of service: ");
+            String date = getStringInput();
+            while (date.length() <= 0) {
+                System.out.print("Enter date of service: ");
+                date = getStringInput();
             }
 
             //Add all user info to the file(appending)
-            printWriter.printf(name, ";", service, ";",saleAmount, ";", date);
-        }
-        catch (FileNotFoundException fnf){
+            printWriter.println(name + ";" + service + ";" + saleAmount + ";"+ date);
+
+            printWriter.close();
+        } catch (FileNotFoundException fnf){
             System.out.println("Sales file does not exist");
-        }
-        catch (IOException exception) {
+        } catch (IOException exception) {
             //e.printStackTrace();
             System.out.println("Could not open file");
         }
-
-
     }
 
-    public static String getName() {
-        return "";
+    public static String getStringInput() {
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine();
+        return userInput;
     }
 
-    public static String getService() {
-        return "";
-    }
+    // I think we can use 1 method to get the name and service
+        /*public static String getName() { return ""; }
+        public static String getService() { return ""; }*/
 
     public static double getAmount(){
-        return 0;
+        Scanner in = new Scanner(System.in);
+        double amount = in.nextDouble();
+        return amount;
     }
 
-    public static String getDate() {
-        return "";
-    }
+    /*public static String getDate() { return ""; }*/
 
     //return true if the user Input is one of the provided service.
     public static boolean isService(String userInput){
-        return true;
+        if (userInput.equals("Dinner") || userInput.equals("Lodging") || userInput.equals("Conference")) {
+            return true;
+        }
+        return false;
     }
 }

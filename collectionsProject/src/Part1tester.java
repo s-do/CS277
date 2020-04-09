4b7ded7e00324f2448b1e9b2a692ed029bb8068d
s-do <48598611+s-do@users.noreply.github.com>
2020-04-08 17:08:17 -0700

    made output a little more descriptive for part 1

--- collectionsProject/src/Part1tester.java
+++ collectionsProject/src/Part1tester.java
@@ -7,6 +7,7 @@ public class Part1tester {
         // command line argument for "q not followed by a u" file
         String inputFileName = args[0];
         File inputFile = new File(inputFileName);
+
         //Second command line argument to choose the data structure
         String dataStructure = args[1];
 
@@ -70,7 +71,7 @@ public class Part1tester {
             wordMap.get("quat");
             wordMap.get("qi");
             long stopSearch = System.nanoTime();
-            System.out.println("Search time: " + (stopSearch - startSearch));
+            System.out.println("Time for searching into Map " + (stopSearch - startSearch) + " nano-seconds");
         }
         catch (FileNotFoundException exception){
             System.out.println("Input file was not found");

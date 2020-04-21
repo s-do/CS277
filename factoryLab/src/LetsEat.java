26e6fa56e014f2f699bef0bf1d8efd6f69b71939
Neo <ln24630@gmail.com>
2020-04-21 16:21:35 -0700

    add tester for factory lab

--- factoryLab/src/LetsEat.java
+++ factoryLab/src/LetsEat.java
@@ -6,19 +6,22 @@ public class LetsEat {
         IceCream cone;
         cone = dairyQueen.orderCone("Chocolate");
         System.out.println("Enjoying some " + cone.getDescription() + " which costs " + cone.cost());
+        System.out.println("");
 
         IceCream cone2;
         cone2 = dairyQueen.orderCone("Vanilla");
         System.out.println("Enjoying some " + cone2.getDescription() + " which costs " + cone2.cost());
+        System.out.println("");
 
         IceCream cone3;
         cone3 = dairyQueen.orderCone("Strawberry");
         System.out.println("Enjoying some " + cone3.getDescription() + " which costs " + cone3.cost());
-
+        System.out.println("");
         //Add toping
         Dessert vanillaCherry = dairyQueen.orderCone("Vanilla");
         vanillaCherry = new Cherry(vanillaCherry);
         System.out.println("Enjoying some " + vanillaCherry.getDescription() + " which costs " + vanillaCherry.cost());
+        System.out.println("");
 
         Dessert chocoWhippedCream = dairyQueen.orderCone("Chocolate");
         chocoWhippedCream = new WhippedCream(chocoWhippedCream);

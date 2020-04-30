a7729ea477d5ac65cafaed25d807166ac2f5a38c
s-do <48598611+s-do@users.noreply.github.com>
2020-04-30 14:31:23 -0700

    added visitor lab

---
+++ visitorLab/src/Fruit.java
@@ -0,0 +1,30 @@
+public class Fruit implements ItemElement
+{
+	private double price;
+	private int amount;
+	private String name;
+
+	public Fruit(String iname,int iamount,double iprice)
+	{
+		name=iname;
+		price=iprice;
+		amount=iamount;
+	}
+
+	public double getPrice()
+	{
+		return price;
+	}
+
+	public int getAmount()
+	{
+		return amount;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+
+}
---
+++ visitorLab/src/ItemElement.java
@@ -0,0 +1,4 @@
+public interface ItemElement
+{
+	public double accept(ShoppingCartVisitor visitor);
+}
